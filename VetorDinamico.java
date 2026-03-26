bef782820af61249d4548f665cb1b88ac32915a5
Rodrigo Bossini <professorbossini@gmail.com>
2026-03-26 16:23:36 -0300

    learn(ap008): redimensiona para adicionar

---
+++ VetorDinamico.java
@@ -0,0 +1,54 @@
+public class VetorDinamico {
+  private int quantidade;
+  private int capacidade;
+  private int [] elementos;
+
+  public VetorDinamico(){
+    quantidade = 0;
+    capacidade = 4;
+    elementos = new int[capacidade];
+  }
+
+  public void adicionar(int e){
+    if(estaCheio()){
+      aumentarCapacidade();  
+    }
+    elementos[quantidade] = e;
+    quantidade++;
+  }
+
+  public void aumentarCapacidade(){
+    var novo = new int[capacidade * 2];
+    for(int i = 0; i < quantidade; i++){
+      novo[i] = elementos[i];
+    }
+    capacidade = novo.length;
+    elementos = novo;
+  }
+
+  public boolean estaVazio(){
+    return quantidade == 0;
+  }
+
+  public boolean estaCheio(){
+    return quantidade == capacidade;
+  }
+
+  @Override
+  public String toString() {
+    //qtde: 2
+    //capacidade: 4
+    //2, 7
+    var sb = new StringBuilder("");
+    sb.append("Qtde: ").append(quantidade);
+    sb.append("\n").append("Cap: ").append(capacidade);
+    if(!estaVazio()){
+      sb.append("\n");
+      for(int i = 0; i < quantidade; i++){
+        sb.append(elementos[i]).append(" ");
+      }
+    }
+    return sb.toString();
+  }
+
+}
