a72fbc46284c84c03f4a39bc4c75497ced52330e
Rodrigo Bossini <professorbossini@gmail.com>
2026-03-26 17:41:54 -0300

    learn(ap008): implementa metodo redimensiona unico

--- VetorDinamico.java
+++ VetorDinamico.java
@@ -2,22 +2,49 @@ public class VetorDinamico {
   private int quantidade;
   private int capacidade;
   private int [] elementos;
+  private static final int CAP_MINIMA = 4;
 
   public VetorDinamico(){
     quantidade = 0;
-    capacidade = 4;
+    capacidade = CAP_MINIMA;
     elementos = new int[capacidade];
   }
 
   public void adicionar(int e){
     if(estaCheio()){
-      aumentarCapacidade();  
+      redimensionar(2);  
     }
     elementos[quantidade] = e;
     quantidade++;
   }
 
-  public void aumentarCapacidade(){
+  public void remover(){
+    if(!estaVazio()){
+      quantidade--;
+      if(capacidade > CAP_MINIMA && quantidade == capacidade / 4){
+        redimensionar(0.5);
+      }
+    }
+  }
+
+  private void redimensionar(double fator){
+    var novo = new int [(int)(capacidade * fator)];
+    for(int i = 0; i < quantidade; i++){
+      novo[i] = elementos[i];
+    }
+    capacidade = novo.length;
+    elementos = novo;
+  }
+
+  private void reduzirCapacidade(){
+    var novo = new int[capacidade / 2];
+    for(int i = 0; i < quantidade; i++){
+      novo[i] = elementos[i];
+    }
+    capacidade = novo.length;
+    elementos = novo;
+  }
+  private void aumentarCapacidade(){
     var novo = new int[capacidade * 2];
     for(int i = 0; i < quantidade; i++){
       novo[i] = elementos[i];
